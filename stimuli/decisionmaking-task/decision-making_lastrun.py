0d2de6baa21ecd8115e8ae3a3307b04e9198e26c
guilhermesaldanha27 <joao.saldanha@staff.uma.pt>
2026-07-21 14:33:31 +0100

    Update decision-making_lastrun.py

--- stimuli/decisionmaking-task/decision-making_lastrun.py
+++ stimuli/decisionmaking-task/decision-making_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2026.1.3),
-    on julho 21, 2026, at 14:08
+    on julho 21, 2026, at 14:28
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 

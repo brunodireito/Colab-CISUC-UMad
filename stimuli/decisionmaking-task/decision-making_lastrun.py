70fed96b0990e28ff442c6046b021d8c05aa6821
guilhermesaldanha27 <joao.saldanha@staff.uma.pt>
2026-07-21 13:51:46 +0000

    Update decision-making_lastrun.py

--- stimuli/decisionmaking-task/decision-making_lastrun.py
+++ stimuli/decisionmaking-task/decision-making_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2026.1.3),
-    on julho 21, 2026, at 14:28
+    on julho 21, 2026, at 13:38
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
@@ -132,7 +132,7 @@ def setupData(expInfo, dataDir=None):
     thisExp = data.ExperimentHandler(
         name=expName, version=expVersion,
         extraInfo=expInfo, runtimeInfo=None,
-        originPath='C:\\Users\\guilh\\Documents\\GitHub\\Colab-CISUC-UMad\\stimuli\\decisionmaking-task\\decision-making_lastrun.py',
+        originPath='C:\\Users\\Laboratorio\\Documents\\GitHub\\Colab-CISUC-UMad\\stimuli\\decisionmaking-task\\decision-making_lastrun.py',
         savePickle=True, saveWideText=True,
         dataFileName=dataDir + os.sep + filename, sortColumns='time'
     )
